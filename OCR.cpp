0346d4fb0e3f6b1d5c76bfada3411db6afa6907e
Aaron Maturen <aaron.maturen@gmail.com>
2011-11-28 15:19:54 -0500

    Added tests for all scanned digits 0-9 with default of ?

--- OCR.cpp
+++ OCR.cpp
@@ -31,9 +31,36 @@ bool OCR::compareScannedCharacters(char scannedCharacter[4][3], char definedChar
 char OCR::convertScannedCharacter(char scannedCharacter[4][3]){
 	char value;
 	char numberOne[4][3] = {{' ',' ',' '},{' ',' ','|'},{' ',' ','|'},{' ',' ',' '}};
+	char numberTwo[4][3] = {{' ','_',' '},{' ','_','|'},{'_','_',' '},{' ',' ',' '}};
+	char numberThree[4][3] = {{' ','_',' '},{' ','_','|'},{' ','_','|'},{' ',' ',' '}};
+	char numberFour[4][3] = {{' ',' ',' '},{'|','_','|'},{' ',' ','|'},{' ',' ',' '}};
+	char numberFive[4][3] = {{' ','_',' '},{'|','_',' '},{' ','_','|'},{' ',' ',' '}};
+	char numberSix[4][3] = {{' ','_',' '},{'|','_',' '},{'|','_','|'},{' ',' ',' '}};
+	char numberSeven[4][3] = {{' ','_',' '},{' ',' ','|'},{' ',' ','|'},{' ',' ',' '}};
+	char numberEight[4][3] = {{' ','_',' '},{'|','_','|'},{'|','_','|'},{' ',' ',' '}};
+	char numberNine[4][3] = {{' ','_',' '},{'|','_','|'},{' ','_','|'},{' ',' ',' '}};
+	char numberZero[4][3] = {{' ','_',' '},{'|',' ','|'},{'|','_','|'},{' ',' ',' '}};
 
 	if(compareScannedCharacters(scannedCharacter, numberOne) == true){
 		value = '1';
+	}else if(compareScannedCharacters(scannedCharacter, numberTwo) == true){
+		value = '2';
+	}else if(compareScannedCharacters(scannedCharacter, numberThree) == true){
+		value = '3';
+	}else if(compareScannedCharacters(scannedCharacter, numberFour) == true){
+		value = '4';
+	}else if(compareScannedCharacters(scannedCharacter, numberFive) == true){
+		value = '5';
+	}else if(compareScannedCharacters(scannedCharacter, numberSix) == true){
+		value = '6';
+	}else if(compareScannedCharacters(scannedCharacter, numberSeven) == true){
+		value = '7';
+	}else if(compareScannedCharacters(scannedCharacter, numberEight) == true){
+		value = '8';
+	}else if(compareScannedCharacters(scannedCharacter, numberNine) == true){
+		value = '9';
+	}else if(compareScannedCharacters(scannedCharacter, numberZero) == true){
+		value = '0';
 	}else{
 		value = '?';
 	}
--- OCRTest.cpp
+++ OCRTest.cpp
@@ -34,9 +34,104 @@ TEST(OCR, TestScannedNumberOne){
 	CHECK_EQUAL(o.convertScannedCharacter(scannedChar),'1');
 }
 
+TEST(OCR, TestScannedNumberTwo){
+	OCR o;
+	char scannedChar[4][3] = {
+			{' ','_',' '},
+			{' ','_','|'},
+			{'_','_',' '},
+			{' ',' ',' '}
+	};
+	CHECK_EQUAL(o.convertScannedCharacter(scannedChar),'2');
+}
 
+TEST(OCR, TestScannedNumberThree){
+	OCR o;
+	char scannedChar[4][3] = {
+			{' ','_',' '},
+			{' ','_','|'},
+			{' ','_','|'},
+			{' ',' ',' '}
+	};
+	CHECK_EQUAL(o.convertScannedCharacter(scannedChar),'3');
+}
 
+TEST(OCR, TestScannedNumberFour){
+	OCR o;
+	char scannedChar[4][3] = {
+			{' ',' ',' '},
+			{'|','_','|'},
+			{' ',' ','|'},
+			{' ',' ',' '}
+	};
+	CHECK_EQUAL(o.convertScannedCharacter(scannedChar),'4');
+}
 
+TEST(OCR, TestScannedNumberFive){
+	OCR o;
+	char scannedChar[4][3] = {
+			{' ','_',' '},
+			{'|','_',' '},
+			{' ','_','|'},
+			{' ',' ',' '}
+	};
+	CHECK_EQUAL(o.convertScannedCharacter(scannedChar),'5');
+}
+
+TEST(OCR, TestScannedNumberSix){
+	OCR o;
+	char scannedChar[4][3] = {
+			{' ','_',' '},
+			{'|','_',' '},
+			{'|','_','|'},
+			{' ',' ',' '}
+	};
+	CHECK_EQUAL(o.convertScannedCharacter(scannedChar),'6');
+}
+
+TEST(OCR, TestScannedNumberSeven){
+	OCR o;
+	char scannedChar[4][3] = {
+			{' ','_',' '},
+			{' ',' ','|'},
+			{' ',' ','|'},
+			{' ',' ',' '}
+	};
+	CHECK_EQUAL(o.convertScannedCharacter(scannedChar),'7');
+}
+
+TEST(OCR, TestScannedNumberEight){
+	OCR o;
+	char scannedChar[4][3] = {
+			{' ','_',' '},
+			{'|','_','|'},
+			{'|','_','|'},
+			{' ',' ',' '}
+	};
+	CHECK_EQUAL(o.convertScannedCharacter(scannedChar),'8');
+}
+
+TEST(OCR, TestScannedNumberNine){
+	OCR o;
+	char scannedChar[4][3] = {
+			{' ','_',' '},
+			{'|','_','|'},
+			{' ','_','|'},
+			{' ',' ',' '}
+	};
+	CHECK_EQUAL(o.convertScannedCharacter(scannedChar),'9');
+}
+
+TEST(OCR, TestScannedNumberZero){
+	OCR o;
+	char scannedChar[4][3] = {
+			{' ','_',' '},
+			{'|',' ','|'},
+			{'|','_','|'},
+			{' ',' ',' '}
+	};
+	CHECK_EQUAL(o.convertScannedCharacter(scannedChar),'0');
+}
 
 
 
