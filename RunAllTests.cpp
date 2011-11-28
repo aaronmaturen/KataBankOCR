0a37688e5848c12395316a6775f738628da4eed0
Aaron Maturen <aaron.maturen@gmail.com>
2011-11-28 14:14:02 -0500

    initial commit... CppUTest Included

---
+++ RunAllTests.cpp
@@ -0,0 +1,13 @@
+/*
+ * RunAllTests.cpp
+ *
+ *  Created on: Nov 28, 2011
+ *      Author: atmature
+ */
+
+
+#include <CppUTest/CommandLineTestRunner.h>
+
+int main(int argc, char **argv) {
+	return CommandLineTestRunner::RunAllTests(argc, argv);
+}
