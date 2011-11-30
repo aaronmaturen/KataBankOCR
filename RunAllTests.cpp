60a53217ade8258a56ce80902c0b1d12e134ded7
Aaron Maturen <aaron.maturen@gmail.com>
2011-11-30 15:48:41 -0500

    Ran production code again to verify output files are the same
    At this point I'm going to call this kata complete... I dont like the
    giant switch in translateScannedCharacter, nor do I like the duplication
    of the number constants in translateScannedCharacter and in
    fixBadCheckSum ... but the code seems to be logically working for all of
    the supplied user stories and test data... so it works.

--- RunAllTests.cpp
+++ RunAllTests.cpp
@@ -1,13 +1,13 @@
-/*
- * RunAllTests.cpp
- *
- *  Created on: Nov 28, 2011
- *      Author: Aaron Maturen
- */
-
-
-#include <CppUTest/CommandLineTestRunner.h>
-
-int main(int argc, char **argv) {
-	return CommandLineTestRunner::RunAllTests(argc, argv);
-}
+///*
+// * RunAllTests.cpp
+// *
+// *  Created on: Nov 28, 2011
+// *      Author: Aaron Maturen
+// */
+//
+//
+//#include <CppUTest/CommandLineTestRunner.h>
+//
+//int main(int argc, char **argv) {
+//	return CommandLineTestRunner::RunAllTests(argc, argv);
+//}
