608e1801a59def3b5c33a7f79eb0e1ff29e49552
Aaron Maturen <aaron.maturen@gmail.com>
2011-11-30 15:10:14 -0500

    added driver for story 3, outputs to story3.txt

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ int main(int argc, char **argv) {
 	std::string line1, line2, line3, line4;
 	std::string accountNumber;
 	std::ifstream filestr ("test.input");
+	std::ofstream story3 ("story3.txt");
 	if(filestr.is_open()){
 		while(filestr.good()){
 			getline (filestr, line1);
@@ -20,19 +21,28 @@ int main(int argc, char **argv) {
 			getline (filestr, line3);
 			getline (filestr, line4);
 
+			//user story 1
 			std::cout << line1 <<std::endl;
 			std::cout << line2 <<std::endl;
 			std::cout << line3 <<std::endl;
 			std::cout << line4 <<std::endl;
 			accountNumber = o.convertScannedLines(line1, line2, line3, line4);
+
+			//user story 2
 			std::cout << std::endl << "=> " << accountNumber;
 			std::cout << ((o.processCheckSum(accountNumber)==0) ? " valid":" invalid") << " checksum" << std::endl;
+
+			//user story 3
+			if (story3.is_open()){
+				story3 << accountNumber << o.validateAccount(accountNumber) << std::endl;
+			}
 		}
 	}
 
 
 
 	filestr.close();
+	story3.close();
 } //end main
 
 
