82322336e3d18d214054c69feb63fba29ba05e31
Aaron Maturen <aaron.maturen@gmail.com>
2011-11-30 15:02:30 -0500

    finished user story 1 and 2

--- RunAllTests.cpp
+++ RunAllTests.cpp
@@ -6,8 +6,8 @@
  */
 
 
-#include <CppUTest/CommandLineTestRunner.h>
-
-int main(int argc, char **argv) {
-	return CommandLineTestRunner::RunAllTests(argc, argv);
-}
+//#include <CppUTest/CommandLineTestRunner.h>
+//
+//int main(int argc, char **argv) {
+//	return CommandLineTestRunner::RunAllTests(argc, argv);
+//}
--- main.cpp
+++ main.cpp
@@ -5,32 +5,34 @@
  *      Author: atmature
  */
 
-//#include "OCR.h"
-//#include <fstream>
-//#include <iostream>
-//int main(int argc, char **argv) {
-//	OCR o;
-//	std::string line1, line2, line3, line4;
-//	std::ifstream filestr ("test.input");
-//	if(filestr.is_open()){
-//		while(filestr.good()){
-//			getline (filestr, line1);
-//			getline (filestr, line2);
-//			getline (filestr, line3);
-//			getline (filestr, line4);
-//
-//			std::cout << "++++++++++"<<std::endl;
-//			std::cout << line1 <<std::endl;
-//			std::cout << line2 <<std::endl;
-//			std::cout << line3 <<std::endl;
-//			std::cout << line4 <<std::endl;
-//			std::cout << o.convertScannedLines(line1, line2, line3, line4) << std::endl;
-//		}
-//	}
-//
-//
-//
-//	filestr.close();
-//} //end main
+#include "OCR.h"
+#include <fstream>
+#include <iostream>
+int main(int argc, char **argv) {
+	OCR o;
+	std::string line1, line2, line3, line4;
+	std::string accountNumber;
+	std::ifstream filestr ("test.input");
+	if(filestr.is_open()){
+		while(filestr.good()){
+			getline (filestr, line1);
+			getline (filestr, line2);
+			getline (filestr, line3);
+			getline (filestr, line4);
+
+			std::cout << line1 <<std::endl;
+			std::cout << line2 <<std::endl;
+			std::cout << line3 <<std::endl;
+			std::cout << line4 <<std::endl;
+			accountNumber = o.convertScannedLines(line1, line2, line3, line4);
+			std::cout << std::endl << "=> " << accountNumber;
+			std::cout << ((o.processCheckSum(accountNumber)==0) ? " valid":" invalid") << " checksum" << std::endl;
+		}
+	}
+
+
+
+	filestr.close();
+} //end main
 
 
